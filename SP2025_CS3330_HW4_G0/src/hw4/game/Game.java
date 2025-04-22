dba38716fe300c35fd6526eb9d816b5c86c9250e
jordanfrancis <jordanfrancis@localhost>
2025-04-22 11:34:49 -0500

    adding code to game that works with the test cases

--- SP2025_CS3330_HW4_G0/src/hw4/game/Game.java
+++ SP2025_CS3330_HW4_G0/src/hw4/game/Game.java
@@ -1,8 +1,11 @@
 package hw4.game;
 
-import hw4.maze.Grid;
 import hw4.player.Player;
 import hw4.maze.*;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 import hw4.player.Movement;
 
 
@@ -31,42 +34,86 @@ public class Game {
 		if (movement == null || player == null || grid == null)
 			return false;
 
-		int rowIndex = grid.currentRow.
-		int colIndex = player.getRow().getCells().indexOf(player.getCell());
+		int rowIndex = grid.getRows().indexOf(player.getCurrentRow());
+		int colIndex = player.getCurrentRow().getCells().indexOf(player.getCurrentCell());
 
 		switch (movement) {
 			case UP:
-				if (player.getCell().getUp() == CellComponents.APERTURE && rowIndex > 0) {
-					player.setRow(grid.getRows().get(rowIndex - 1));
-					player.setCell(player.getRow().getCells().get(colIndex));
+				if (player.getCurrentCell().getUp() == CellComponents.APERTURE && rowIndex > 0) {
+					player.setCurrentRow(grid.getRows().get(rowIndex - 1));
+					player.setCurrentCell(player.getCurrentRow().getCells().get(colIndex));
 					return true;
 				}
 				break;
 			case DOWN:
-				if (player.getCell().getDown() == CellComponents.APERTURE && rowIndex < grid.getRows().size() - 1) {
-					player.setRow(grid.getRows().get(rowIndex + 1));
-					player.setCell(player.getRow().getCells().get(colIndex));
+				if (player.getCurrentCell().getDown() == CellComponents.APERTURE && rowIndex < grid.getRows().size() - 1) {
+					player.setCurrentRow(grid.getRows().get(rowIndex + 1));
+					player.setCurrentCell(player.getCurrentRow().getCells().get(colIndex));
 					return true;
 				}
 				break;
 			case LEFT:
-				if (player.getCell().getLeft() == CellComponents.APERTURE && colIndex > 0) {
-					player.setCell(player.getRow().getCells().get(colIndex - 1));
+				if (player.getCurrentCell().getLeft() == CellComponents.APERTURE && colIndex > 0) {
+					player.setCurrentCell(player.getCurrentRow().getCells().get(colIndex - 1));
 					return true;
 				}
 				break;
 			case RIGHT:
-				if (player.getCell().getRight() == CellComponents.APERTURE && colIndex < player.getRow().getCells().size() - 1) {
-					player.setCell(player.getRow().getCells().get(colIndex + 1));
+				if (player.getCurrentCell().getRight() == CellComponents.APERTURE && colIndex < player.getCurrentRow().getCells().size() - 1) {
+					player.setCurrentCell(player.getCurrentRow().getCells().get(colIndex + 1));
 					return true;
 				}
 				break;
 		}
 		return false;
 	}
+	public Grid createRandomGrid(int size) {
+		if (size < 3 || size > 7) {
+			return null;
+		}
 
-	private Grid createRandomGrid(int size) {
-		return null;
+		ArrayList<Row> rows = new ArrayList<>();
+		Random random = new Random();
+		int exitRow = random.nextInt(size);
+
+		for (int i = 0; i < size; i++) {
+			ArrayList<Cell> cells = new ArrayList<>();
+			for (int j = 0; j < size; j++) {
+				CellComponents left = (j == 0 && i == exitRow) ? CellComponents.EXIT : randomComponent();
+				CellComponents right = (j == size - 1) ? CellComponents.WALL : randomComponent();
+				CellComponents up = (i == 0) ? CellComponents.WALL : randomComponent();
+				CellComponents down = (i == size - 1) ? CellComponents.WALL : randomComponent();
+				cells.add(new Cell(left, right, up, down));
+			}
+			rows.add(new Row(cells));
+		}
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size - 1; j++) {
+				// horizontal
+				Cell left = rows.get(i).getCells().get(j);
+				Cell right = rows.get(i).getCells().get(j + 1);
+				right.setLeft(left.getRight());
+
+				// vertical
+				if (i < size - 1) {
+					Cell top = rows.get(i).getCells().get(j);
+					Cell bottom = rows.get(i + 1).getCells().get(j);
+					bottom.setUp(top.getDown());
+				}
+			}
+		}
+
+		return new Grid(rows);
+	}
+
+	private CellComponents randomComponent() {
+		return Math.random() > 0.5 ? CellComponents.APERTURE : CellComponents.WALL;
+	}
+
+	@Override
+	public String toString() {
+		return "Game [grid=" + grid + "]";
 	}
 
 }
