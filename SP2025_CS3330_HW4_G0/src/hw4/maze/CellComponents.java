1334e55ebc83c74f370a9e4c99e2a6f68819abe7
jordanfrancis <jordanfrancis@localhost>
2025-04-22 14:52:38 -0500

    adding comments to the key word enums

--- SP2025_CS3330_HW4_G0/src/hw4/maze/CellComponents.java
+++ SP2025_CS3330_HW4_G0/src/hw4/maze/CellComponents.java
@@ -1,6 +1,8 @@
 package hw4.maze;
 
 public enum CellComponents {
+	
+	//key words for how the player can move 
 	WALL, APERTURE, EXIT
 
 }
--- SP2025_CS3330_HW4_G0/src/hw4/player/Movement.java
+++ SP2025_CS3330_HW4_G0/src/hw4/player/Movement.java
@@ -2,6 +2,7 @@ package hw4.player;
 
 public enum Movement {
 	
+	//key words for the movement available in the maze
 	UP, DOWN, LEFT, RIGHT
 
 }
